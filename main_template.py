6cb08a5a17866947c79f25a368ee1f61f3953cfe
Reid Butson <100561195@oncampus.local>
2018-03-28 11:23:56 -0400

    Template
    Main Template Created

---
+++ main_template.py
@@ -0,0 +1,16 @@
+
+while True:
+    pass
+    #-------TARGET STUFF---------
+        #-------DRAW TARGETS-----
+
+    #-------AGENT STUFF----------
+        #------DRAW AGENT--------
+        #------MOVE AGENT--------
+            #---IS WINNER?-------
+
+    #-----BREAK TO WIN SEQUENCE--
+
+    #----REFRESH TERMINAL--------
+
+#-----WIN SEQUENCE
